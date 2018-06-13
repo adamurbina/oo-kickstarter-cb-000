f86b7412d146a3f3571c8ec1da4de950b5cd307c
Adam Urbina <adamurbina@gmail.com>
2018-06-13 00:30:37 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,14 @@
 class Project
 
-    attr_accessor :title
+    attr_accessor :title, :backed_projects
 
     def initialize(title)
         @title = title
+        @backed_projects = []
     end
+
+    def back_project(title)
+        @backed_projects << title
+    end
+
 end
